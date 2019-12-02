e23e04c4dbbd28e78e7a13649e9899be233bf6f5
crspradlin <cs12230@georgiasouthern.edu>
2019-12-02 12:16:47 -0500

    pushing for home server

--- start.py
+++ start.py
@@ -1,17 +1,7 @@
-import sys
+
 import pyshark
-import csv
-import json
-
-packets = {};
-packets = json.loads(packets)
-with open('csv_capture.csv') as fh:
-    rd=csv.DictReader(fh, delimiter=',')
-    count = 0
-    for row in rd:
-        packets.append(json.dumps(row))
-        count = count + 1 
-
-print(packets)
-
-sys.stdout.flush()
+capture = pyshark.LiveCapture(interface=r'\Device\NPF_{D41D8EE1-2739-4FA1-8873-024D3F68E9E1}')
+capture.sniff(timeout=1)
+pkts = [pkt for pkt in capture._packets]
+print(len(capture))
+capture.close()
